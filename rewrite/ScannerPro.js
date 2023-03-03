038d1aece762442db549fbc6e912e10f358e0680
超级大魔王 <50542287+BigBigBigBigBigDevil@users.noreply.github.com>
2023-03-03 23:26:19 +0800

    Update ScannerPro.js

--- rewrite/ScannerPro.js
+++ rewrite/ScannerPro.js
@@ -2,15 +2,8 @@
 
 /******************************
 
-脚本功能：Scanner Pro+解锁订阅
-下载地址：http://mtw.so/6bRcSt
-软件版本：8.10
-脚本作者：彭于晏
-更新时间：2022-9-11
-问题反馈：QQ+89996462
-QQ会员群：779392027
-TG反馈群：https://t.me/plus8889
-使用声明：⚠️此脚本仅供学习与交流，请勿转载与贩卖！⚠️⚠️⚠️
+Scanner Pro+解锁订阅
+https://raw.githubusercontent.com/89996462/Quantumult-X/main/ycdz/Scanner.js
 
 *******************************
 
