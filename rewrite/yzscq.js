bea1a47ddf2a5143178fcc6f5adce3c3de391809
超级大魔王 <50542287+BigBigBigBigBigDevil@users.noreply.github.com>
2023-03-04 00:12:37 +0800

    Update yzscq.js

--- rewrite/yzscq.js
+++ rewrite/yzscq.js
@@ -7,7 +7,7 @@ https://raw.githubusercontent.com/89996462/Quantumult-X/main/ycdz/yzscq.js
 
 [rewrite_local]
 
-^https:\/\/buy\.itunes\.apple\.com\/verifyReceipt url script-response-body https://raw.githubusercontent.com/89996462/Quantumult-X/main/ycdz/yzscq.js
+^https:\/\/buy\.itunes\.apple\.com\/verifyReceipt url script-response-body https://raw.githubusercontent.com/BigBigBigBigBigDevil/QuantumultX_configuration/main/rewrite/yzscq.js
 
 [mitm] 
 
