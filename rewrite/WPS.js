991b4288bd38e6c3b9c7228b64c0504be180ec42
超级大魔王 <50542287+BigBigBigBigBigDevil@users.noreply.github.com>
2023-03-27 22:00:07 +0800

    Update WPS.js

--- rewrite/WPS.js
+++ rewrite/WPS.js
@@ -1,4 +1,17 @@
 /***
 wps会员
 from: https://raw.githubusercontent.com/510004015/Quantumult_X/Remote/Premium/WPSOffice.js
+
+
+[rewrite_local]
+
+# > Old
+^https:\/\/account\.wps\.cn\/api\/users\/\d+\/overview url script-response-body https://raw.githubusercontent.com/510004015/Quantumult_X/Remote/Premium/WPSOffice.js
+# > New
+^https:\/\/vas\.wps\.cn\/query\/api\/v\d\/list_purchase_info url script-response-body https://raw.githubusercontent.com/510004015/Quantumult_X/Remote/Premium/WPSOffice.js
+
+[mitm] 
+
+hostname = account.wps.cn, vas.wps.cn
+
 **/
