7785ec9781279704c55cee4361c054a4c311c221
超级大魔王 <50542287+BigBigBigBigBigDevil@users.noreply.github.com>
2025-11-03 22:08:54 +0800

    Delete rewrite directory

--- rewrite/dicallapp.js
+++
@@ -1,3 +0,0 @@
-body = $response.body.replace(/\"TFlag":\d+/g, '\"TFlag":1').replace(/\"UFlag":\d+/g, '\"UFlag":1').replace(/\"UserVipEnd":"(.*?)"/g, '\"UserVipEnd":"2099-09-09 09:09:09"')
-
-$done({body});
--- rewrite/xxlp.js
+++
@@ -1,2 +0,0 @@
-var body = $response.body.replace(/"expiresDateMs":\d+/g,'"expiresDateMs":3670811527000')
-$done({ body });
